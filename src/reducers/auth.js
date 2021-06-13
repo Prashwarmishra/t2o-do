a8e7f7ea3d58b920c9a9d54927152d1b74430107
Prashwar Mishra <prashwarmishra@gmail.com>
2021-06-13 16:02:05 +0530

    add reducer for login

---
+++ src/reducers/auth.js
@@ -0,0 +1,38 @@
+import {
+  LOGIN_FAILURE,
+  LOGIN_START,
+  LOGIN_SUCCESS,
+} from '../actions/actionTypes';
+
+const initialAuthState = {
+  user: {},
+  error: null,
+  inProgress: false,
+  isLoggedin: false,
+};
+
+export default function auth(state = initialAuthState, action) {
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        inProgress: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: action.user,
+        error: null,
+        inProgress: false,
+        isLoggedin: true,
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        error: action.error,
+        inProgress: false,
+      };
+    default:
+      return state;
+  }
+}
